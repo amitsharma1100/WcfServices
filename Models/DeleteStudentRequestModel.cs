aa023e882a23712800f71c12a6b84c35523f595b
amitsharma1100 <sharma.amit1100@gmail.com>
2018-01-07 17:37:31 +0530

    Create DeleteStudentRequestModel.cs

---
+++ Models/DeleteStudentRequestModel.cs
@@ -0,0 +1,13 @@
+namespace WcfServiceLibrary.Models
+{
+    /// <summary>
+    ///     Delete student request model class.
+    /// </summary>
+    public class DeleteStudentRequestModel
+    {
+        /// <summary>
+        ///     Gets or sets the ID.
+        /// </summary>
+        public int Id { get; set; }
+    }
+}
